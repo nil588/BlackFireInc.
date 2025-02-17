23c5b7b36cda4e503c4b2d7e57e27e1ca6d86762
Nilesh Venkateshan <77702517+nil588@users.noreply.github.com>
2025-02-17 11:40:25 -0800

    UserModel.js

---
+++ src/server/config/src/server/models/UserModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    adminAccess: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+export const UserModel = mongoose.model('User', userSchema);
